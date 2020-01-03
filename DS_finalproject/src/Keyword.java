ab4c1056e256d138b29aa454d5976f7c98755fc4
tiffantchiao <45808505+tiffantchiao@users.noreply.github.com>
2020-01-03 10:35:29 +0800

    keyword加weight

--- DS_finalproject/src/Keyword.java
+++ DS_finalproject/src/Keyword.java
@@ -5,14 +5,16 @@
 public class Keyword {
 	public String name;
     public int count;
+    public float weight;
     
     public Keyword(String name,int count){
 		this.name = name;
 		this.count =count;
+		this.weight =weight;
     }
     
     @Override
     public String toString(){
-    	return "["+name+","+count+"]";
+    	return "["+name+","+count+","+weight+"]";
     }
 }
