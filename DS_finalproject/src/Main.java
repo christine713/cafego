bcacac32a9aaffb5eab6151f6fdd75dbcd642145
tiffantchiao <45808505+tiffantchiao@users.noreply.github.com>
2020-01-05 16:34:23 +0800

    初步的main

--- DS_finalproject/src/Main.java
+++ DS_finalproject/src/Main.java
@@ -1,19 +1,30 @@
 
+import java.util.HashMap;
 import java.util.Scanner;
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Please type: URL Keyword");
+		//把hw3改成輸入keyword 連接google 算出count
+		System.out.println("Please type: 捷運站名");
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNextLine()){
-		    String urlStr = sc.next();
-		    String keyword = sc.next();
-		   
-		    WordCounter counter = new WordCounter(urlStr);
+		    String station = sc.next();
+			try {
+				HashMap<String,String> query=new GoogleQuery("NCCU").query();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		    WordCounter counter = new WordCounter(query);
 		    System.out.println(counter.countKeyword(keyword));
 		}
+		
+
+
 
 }
 }
