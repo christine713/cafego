d23c1723b142f68c2b7a7dcc1353f6e7b4512717
Peggy <Peggy@DESKTOP-V3913HR>
2020-01-08 20:15:07 +0800

    7

--- DS_finalproject/src/Main.java
+++ DS_finalproject/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException ,NullPointerException{
 		// TODO Auto-generated method stub
-		//WHYYYYYYYY 2345678910123456
+		//WHYYYYYYYY 23456789101234567s
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Please type the number of keywords:");
