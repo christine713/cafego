8c24f2b8484f4c321ff3d948a7d14d395e7caf1b
Peggy <Peggy@DESKTOP-V3913HR>
2020-01-08 21:55:43 +0800

    e

--- DS_finalproject/src/Main.java
+++ DS_finalproject/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException ,NullPointerException{
 		// TODO Auto-generated method stub
-		//WHYYYYYYYY 23456789101234567s8901
+		//WHYYYYYYYY 23456789101234567s890
 		
 		Scanner sc = new Scanner(System.in);
 		
