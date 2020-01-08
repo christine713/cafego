d23e91e1405dc6036414bd3746a3732ac1f28eb3
Peggy <Peggy@DESKTOP-V3913HR>
2020-01-08 22:28:57 +0800

    f

--- DS_finalproject/src/Main.java
+++ DS_finalproject/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException ,NullPointerException{
 		// TODO Auto-generated meth 
-		//WH
+		//
 		
 		Scanner sc = new Scanner(System.in);
 		
