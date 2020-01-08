5e155d9d556a055a3d7f5fac4ffb855ce07432df
Peggy <Peggy@DESKTOP-V3913HR>
2020-01-08 22:29:54 +0800

    f

--- DS_finalproject/src/Main.java
+++ DS_finalproject/src/Main.java
@@ -12,7 +12,7 @@ import javax.lang.model.util.Elements;
 public class Main {
 
 	public static void main(String[] args) throws IOException ,NullPointerException{
-		// TODO Auto-generated 
+		// TODO Auto-generat
 		
 	
 		Scanner sc = new Scanner(System.in);
